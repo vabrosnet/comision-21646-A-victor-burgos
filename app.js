ceed2a5042b62973618fe94fa4a063fa0bb45997
vabrosnet <vabros@hotmail.com>
2023-09-10 20:33:56 -0300

    middlewares

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors'
+import helmet from 'helmet'
 import 'dotenv/config';
 import { startDb } from './src/database/database.js'
 
-const app = express();
 
 //settings
+const app = express();
 const port = process.env.PORT;
 
+
 //middlewares
+app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'));
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
 
-
-//Server listening
+// Iniciando servidor y verificando database
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
     startDb();
-})
-
-// // export default app;
+});
--- index.js
+++
@@ -1,9 +0,0 @@
-// import app from './app';
-
-// const main = () =>{
-//     app.listen(app.get("port"));
-//     console.log(`Server listening on port ${app.get("port")}`);
-// };
-
-
-// main();
